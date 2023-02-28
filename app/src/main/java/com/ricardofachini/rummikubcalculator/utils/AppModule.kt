6c0b76de48a1703927ed42f280582516c5b1abb6
Ricardo Fachini <ricardofachinisouza@gmail.com>
2023-02-28 11:10:50 -0300

    add hilt app module

---
+++ app/src/main/java/com/ricardofachini/rummikubcalculator/utils/AppModule.kt
@@ -0,0 +1,14 @@
+package com.ricardofachini.rummikubcalculator.utils
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+}
