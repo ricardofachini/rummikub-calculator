04c6ce983994daa322fa25168588b8375db005e1
Ricardo Fachini <ricardofachinisouza@gmail.com>
2023-02-28 11:11:03 -0300

    add hilt main app

--- app/src/main/java/com/ricardofachini/rummikubcalculator/RummikubCalculatorApplication.kt
+++ app/src/main/java/com/ricardofachini/rummikubcalculator/RummikubCalculatorApplication.kt
@@ -1,7 +1,9 @@
 package com.ricardofachini.rummikubcalculator
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 class RummikubCalculatorApplication: Application() {
 
     override fun onCreate() {
