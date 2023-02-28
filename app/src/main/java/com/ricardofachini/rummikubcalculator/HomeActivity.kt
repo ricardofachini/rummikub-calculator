06e1079052ad5d3bfe7c93426c16ef0e6f9d05ca
Ricardo Fachini <ricardofachinisouza@gmail.com>
2023-02-28 16:06:44 -0300

    new match button onclicklisterner on home activity

--- app/src/main/java/com/ricardofachini/rummikubcalculator/HomeActivity.kt
+++ app/src/main/java/com/ricardofachini/rummikubcalculator/HomeActivity.kt
@@ -1,11 +1,14 @@
 package com.ricardofachini.rummikubcalculator
 
+import android.content.Intent
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
 import com.ricardofachini.rummikubcalculator.databinding.ActivityHomeBinding
+import com.ricardofachini.rummikubcalculator.match.MatchActivity
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class HomeActivity: AppCompatActivity() {
     private lateinit var binding: ActivityHomeBinding
 
@@ -13,9 +16,17 @@ class HomeActivity: AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setupView()
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         return super.onCreateOptionsMenu(menu)
+    }
+
+    private fun setupView() {
+        binding.newMatchButton.setOnClickListener {
+            val switchActivityIntent = Intent(this, MatchActivity::class.java)
+            startActivity(switchActivityIntent)
+        }
     }
 }
