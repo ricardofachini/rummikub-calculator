f04229da96407c53b1e7ede805c24de11cccdc54
Ricardo Fachini <ricardofachinisouza@gmail.com>
2023-03-11 19:26:09 -0300

    implements database singleton behavior

--- data/src/main/java/com/ricardofachini/rummikubcalculator/data/local/AppDatabase.kt
+++ data/src/main/java/com/ricardofachini/rummikubcalculator/data/local/AppDatabase.kt
@@ -1,14 +1,41 @@
 package com.ricardofachini.rummikubcalculator.data.local
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
 import com.ricardofachini.rummikubcalculator.data.local.dao.MatchDao
 import com.ricardofachini.rummikubcalculator.data.local.dao.PlayerDao
 import com.ricardofachini.rummikubcalculator.data.local.entity.MatchEntityLocal
 import com.ricardofachini.rummikubcalculator.data.local.entity.PlayerEntityLocal
 
 @Database(entities = [PlayerEntityLocal::class, MatchEntityLocal::class], version = 1)
-abstract class AppDatabase {
+abstract class AppDatabase: RoomDatabase() {
     abstract fun matchDao(): MatchDao
 
     abstract fun playerDao(): PlayerDao
+
+    companion object {
+        @Volatile
+        var databaseInstance: AppDatabase? = null
+
+        fun getInstance(context: Context): AppDatabase {
+            return databaseInstance ?: synchronized(this) {
+                databaseInstance ?: buildDatabase(context).also {
+                    databaseInstance = it
+                }
+            }
+
+        }
+
+        private fun buildDatabase(context: Context): AppDatabase {
+            return Room.databaseBuilder(context, AppDatabase::class.java, "RummikubCalcDatabase.db")
+                .addCallback(
+                    object : RoomDatabase.Callback() {
+                    }
+                )
+                .build()
+        }
+    }
 }
